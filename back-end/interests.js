cbf8339cfd8a415b2cec6007ae26cb28b5e1f71d
Jlahmy <jlahmy@yahoo.com>
2018-05-14 18:29:20 -0400

    updating events

--- back-end/interests.js
+++ back-end/interests.js
@@ -2,16 +2,28 @@
 
 let museums = {
     '1': {
-
+        name: 'les Fusiliers Mont-Royal Museum',
+        description: 'Le régiment des Fusiliers Mont-Royal, tel qu’on le connaît aujourd’hui, constitue le plus ancien bataillon francophone de milice à avoir survécu jusqu’ici.',
+        coordinates: { lat: 45.518012, long: -73.572168 },
+        address: '3721 Avenue Henri Julien'
     },
     '2': {
-
+        name: 'Jacques Rousseau House',
+        description:'House, of Jean-Jacques Rousseau',
+        coordinates:{lat:45.520181 , long:-73.583276 },
+        address:'4333 Coloniale Ave'
     },
     '3': {
-
+        name:'Rone Face Mural',
+        description:'A mural of a Face Artist Rone',
+        coordinates:{lat:45.516604 , long: -73.576828},
+        address:'3897 St Dominique St'
     },
-    '4':{
-
+    '4': {
+        name:'Montreal Jewish Museum',
+        description:'The Museum of Jewish Montreal (MJM) is a not-for-profit organization that collects, maps, and shares the history and experiences of the Montreal Jewish community online and through walking tours, exhibits, and other public programming.',
+        coordinates:{lat:45.516949 , long:-73.579716 },
+        address:'4040 St Laurent Blvd R01'
     }
 
 }
@@ -26,8 +38,8 @@ let parks = {
     '3': {
 
     },
-    '4':{
-        
+    '4': {
+
     }
 
 }
@@ -47,7 +59,7 @@ let bars = {
 
         },
         '4': {
-            
+
         }
 
     }
--- back-end/restaurants.js
+++ back-end/restaurants.js
@@ -3,18 +3,30 @@
 let asian = {
     expensive: {
         '1': {
-
+            name: 'Sushi Momo Vegetalien',
+            description: 'Rustic-chic bistro specializing in creative vegan sushi rolls & Japanese entrees.',
+            coordinates: {lat:45.518112 , long:-73.569495},
+            address: '3609 St Denis Str'
         },
         '2': {
-
+            name: 'Mito Sushi',
+            description: 'Puisqu’on ne fait rien d’ordinaire, nos Itamae ont revisité les sushis incontournables en leur ajoutant leur touche de créativité ; les subtilités de ces indispensables vous feront craquer.',
+            coordinates: { lat: 45.518845, long: -73.587720},
+            address: '101 Mont-Royal Ave W'
         }
     },
     cheap: {
         '3': {
-
+            name: 'Ramen Ya',
+            description: 'Japanese soups, noodle dishes & sake in a casual setting with wooden touches & a wraparound counter.',
+            coordinates: { lat: 45.518514, long: -73.583266},
+            address: '4274 St Laurent Blvd'
         },
         '4': {
-
+            name: 'Ichi Sushi',
+            description: 'Extensive menu of creative sushi rolls (some in soy-paper wraps) & miso soups to take away.',
+            coordinates: { lat: 45.520494, long: -73.586166},
+            address: '23 Mont-Royal Ave E'
         }
     }
 }
