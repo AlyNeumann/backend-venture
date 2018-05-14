b1f43e30c4f90a59d3ba7cd2d5557f33946b4992
Jlahmy <jlahmy@yahoo.com>
2018-05-14 19:10:15 -0400

    updated endpoints

--- back-end/api.js
+++ back-end/api.js
@@ -9,6 +9,29 @@ app.use(express.static('public'))
 
 app.use(bodyParser.raw({ type: '*/*', limit: '50mb' }))
 
-//end points here
+/*End Points Here*/
+
+//Gets the username 
+app.get('/userName', (req, res) => {
+
+})
+//Receives the first activity body
+app.post('/userPreferenceFirstActivity', (req, res) => {
+
+})
+//Receives the second activity body
+app.get('/getSecondActivity', (req,res) => {
+
+})
+//Receives the third activity (restaurant)
+app.get('/getThirdActivity', (req,res) => {
+
+})
+//Receives the fourth activity
+app.get('/getFourthActivity', (req,res) => {
+
+})
+
 
 app.listen(4000, () => console.log('Listening on port 4000!'))
+
