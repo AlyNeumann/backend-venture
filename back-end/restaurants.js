8c31f1b89821b4adcca03050b52aa4476f95d7fe
Allison Neumann <allisonneumann@Allisons-MacBook-Pro.local>
2018-05-14 17:55:03 -0400

    template done for real omg

--- back-end/restaurants.js
+++ back-end/restaurants.js
@@ -2,28 +2,45 @@
 
 let asian = {
     expensive: {
+        '1': {
 
+        },
+        '2': {
+
+        }
     },
     cheap: {
+        '3': {
 
+        },
+        '4': {
+
+        }
     }
 }
 
 let latinMex = {
     expensive: {
-        '3': {
+        '5': {
             name: 'T&T Tacos et Tortas',
             description: 'Mexican food & Cocktail/Wine Bar',
             coordinates: { lat: 45.517468, long: -73.582390 },
             address: '51 Rachel St W'
         },
-        '4': {
-            
-        }
+        '6': {
 
+
+        }
 
     },
     cheap: {
+        '7': {
+
+        },
+        '8': {
+
+        }
 
     }
 }
+   
