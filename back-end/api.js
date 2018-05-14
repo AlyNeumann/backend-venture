4726354df4618a9695fd393616840d0c795a8249
Allison Neumann <allisonneumann@Allisons-MacBook-Pro.local>
2018-05-14 15:39:11 -0400

    first commit

---
+++ back-end/api.js
@@ -0,0 +1,12 @@
+const venture = require('./venture')
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+const fs = require('fs')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.raw({ type: '*/*', limit: '50mb' }))
+
+app.listen(4000, () => console.log('Listening on port 4000!'))
