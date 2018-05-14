f973af55b581ed80a90e187e075c28b14abd9197
Jlahmy <jlahmy@yahoo.com>
2018-05-14 18:33:44 -0400

    more event additions

--- back-end/interests.js
+++ back-end/interests.js
@@ -9,21 +9,21 @@ let museums = {
     },
     '2': {
         name: 'Jacques Rousseau House',
-        description:'House, of Jean-Jacques Rousseau',
-        coordinates:{lat:45.520181 , long:-73.583276 },
-        address:'4333 Coloniale Ave'
+        description: 'House, of Jean-Jacques Rousseau',
+        coordinates: { lat: 45.520181, long: -73.583276 },
+        address: '4333 Coloniale Ave'
     },
     '3': {
-        name:'Rone Face Mural',
-        description:'A mural of a Face Artist Rone',
-        coordinates:{lat:45.516604 , long: -73.576828},
-        address:'3897 St Dominique St'
+        name: 'Rone Face Mural',
+        description: 'A mural of a Face Artist Rone',
+        coordinates: { lat: 45.516604, long: -73.576828 },
+        address: '3897 St Dominique St'
     },
     '4': {
-        name:'Montreal Jewish Museum',
-        description:'The Museum of Jewish Montreal (MJM) is a not-for-profit organization that collects, maps, and shares the history and experiences of the Montreal Jewish community online and through walking tours, exhibits, and other public programming.',
-        coordinates:{lat:45.516949 , long:-73.579716 },
-        address:'4040 St Laurent Blvd R01'
+        name: 'Montreal Jewish Museum',
+        description: 'The Museum of Jewish Montreal (MJM) is a not-for-profit organization that collects, maps, and shares the history and experiences of the Montreal Jewish community online and through walking tours, exhibits, and other public programming.',
+        coordinates: { lat: 45.516949, long: -73.579716 },
+        address: '4040 St Laurent Blvd R01'
     }
 
 }
@@ -47,12 +47,29 @@ let parks = {
 let bars = {
     expensive: {
         '1': {
-
+            name: 'La P’tite grenouille',
+            description: 'Festive late-night bar offering poutine, burgers & other hearty pub fare, live bands & DJs.',
+            coordinates: { lat: 45.512884, long: -73.569970 },
+            address: '3435 St Laurent Blvd'
         },
         '2': {
-
+            name: 'Blizzarts',
+            description: 'Bar with themed DJ nights (ranging from reggae to rap), plus candlelit booths & dance floors.',
+            coordinates: { lat: 45.516634, long: -73.578900 },
+            address: '3956 St Laurent Blvd'
+        },
+        '3': {
+            name: 'Fitzroy',
+            description: "15 tables de billard et ses 3 tables de baby-foot! Le Fitzroy, c'est le rendez-vous des joueurs de billard et le rassembleur des amateurs d’événements sportifs de tous genres",
+            coordinates: { lat: 45.525248, long: -73.581954 },
+            address: '551 Mont-Royal Ave E'
+        },
+        '4': {
+            name: 'McKibbins irish pub',
+            description: 'Great service, awesome food, great prices and good selection of beer.',
+            coordinates: { lat: 45.513410, long: -73.571244 },
+            address: '3515 St Laurent Blvd'
         }
-
     },
     cheap: {
         '3': {
