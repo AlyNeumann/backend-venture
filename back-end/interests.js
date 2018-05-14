513daacfc7e55067b6e67f332b0c39697c3934f7
Allison Neumann <allisonneumann@Allisons-MacBook-Pro.local>
2018-05-14 19:25:56 -0400

    interests work

--- back-end/interests.js
+++ back-end/interests.js
@@ -43,40 +43,59 @@ let parks = {
     }
 
 }
+ //replace expensive bars with the correct list
 
 let bars = {
     expensive: {
         '1': {
+            name: 'The Wiggle Room',
+            description: 'Voted best cocktails in Montreal, nightly burlesque shows',
+            coordinates: { lat: 45.516096, long: -73.577712},
+            address: '3874 St Laurent Blvd'
+        },
+        '2': {
+            name: 'Bootlegger',
+            description: 'Fancy cocktails, live music, 1920s vibes',
+            coordinates: { lat: 45.513194, long: -73.570679 },
+            address: '3956 St Laurent Blvd'
+        },
+        '3': {
+            name: '',
+            description: '',
+            coordinates: { lat: 0, long: 0},
+            address: ''
+        },
+        '4': {
+            name: '',
+            description: '',
+            coordinates: { lat: 0, long: 0 },
+            address: ''
+        }
+    },
+    cheap: {
+        '5': {
             name: 'La P’tite grenouille',
             description: 'Festive late-night bar offering poutine, burgers & other hearty pub fare, live bands & DJs.',
             coordinates: { lat: 45.512884, long: -73.569970 },
             address: '3435 St Laurent Blvd'
         },
-        '2': {
+        '6': {
             name: 'Blizzarts',
             description: 'Bar with themed DJ nights (ranging from reggae to rap), plus candlelit booths & dance floors.',
             coordinates: { lat: 45.516634, long: -73.578900 },
             address: '3956 St Laurent Blvd'
         },
-        '3': {
+        '7': {
             name: 'Fitzroy',
             description: "15 tables de billard et ses 3 tables de baby-foot! Le Fitzroy, c'est le rendez-vous des joueurs de billard et le rassembleur des amateurs d’événements sportifs de tous genres",
             coordinates: { lat: 45.525248, long: -73.581954 },
             address: '551 Mont-Royal Ave E'
         },
-        '4': {
+        '8': {
             name: 'McKibbins irish pub',
             description: 'Great service, awesome food, great prices and good selection of beer.',
             coordinates: { lat: 45.513410, long: -73.571244 },
             address: '3515 St Laurent Blvd'
-        }
-    },
-    cheap: {
-        '3': {
-
-        },
-        '4': {
-
         }
 
     }
