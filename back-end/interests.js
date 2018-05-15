ea938079a691bea678915296c85ddd76d7906fe5
Allison Neumann <allisonneumann@Allisons-MacBook-Pro.local>
2018-05-15 10:31:15 -0400

    backend restructured with arrays

--- back-end/interests.js
+++ back-end/interests.js
@@ -1,137 +1,137 @@
 //interests data stored here
 
-let museums = {
-    '1': {
+let museums = [
+    {
         name: 'les Fusiliers Mont-Royal Museum',
         description: 'Le régiment des Fusiliers Mont-Royal, tel qu’on le connaît aujourd’hui, constitue le plus ancien bataillon francophone de milice à avoir survécu jusqu’ici.',
         coordinates: { lat: 45.518012, long: -73.572168 },
         address: '3721 Avenue Henri Julien'
     },
-    '2': {
+    {
         name: 'Jacques Rousseau House',
         description: 'House, of Jean-Jacques Rousseau',
         coordinates: { lat: 45.520181, long: -73.583276 },
         address: '4333 Coloniale Ave'
     },
-    '3': {
+    {
         name: 'Rone Face Mural',
         description: 'A mural of a Face Artist Rone',
         coordinates: { lat: 45.516604, long: -73.576828 },
         address: '3897 St Dominique St'
     },
-    '4': {
+    {
         name: 'Montreal Jewish Museum',
         description: 'The Museum of Jewish Montreal (MJM) is a not-for-profit organization that collects, maps, and shares the history and experiences of the Montreal Jewish community online and through walking tours, exhibits, and other public programming.',
         coordinates: { lat: 45.516949, long: -73.579716 },
         address: '4040 St Laurent Blvd R01'
     }
 
-}
+]
 
-let parks = {
-    '1': {
+let parks = [
+    {
         name: 'Jeanne-Mance Park',
         description: 'Climb the famous Mont Royal mountain, or just enjoy the view.',
         coordinates: { lat: 45.515245, long: -73.583023},
         address: 'Av du Parc & Avenue du Mont-Royal'
     },
-    '2': {
+    {
         name: 'Sir Wilfrid-Laurier Park',
         description: 'Roomy park featuring an outdoor winning pool, playgrounds & ballfields, and free WIFI',
         coordinates: { lat: 45.530053, long: -73.586840},
         address: 'Mentana St & Avenue Laurier E'
     },
-    '3': {
+    {
         name: 'Parc du Portugal',
         description: 'Small park with beautiful gazebo',
         coordinates: { lat: 45.519062, long: -73.583885},
         address: 'Marie-Anne St E & St Laurent Blvd'
     },
-    '4': {
+    {
         name: 'Parc des Amériques',
         description: 'Small park, hosting site of the famous Montreal Fringe Festival',
         coordinates: { lat: 45.518372, long: -73.581798},
         address: 'Rue Rachel E & St Laurent Blvd'
     }
 
-}
+]
 
 let bars = {
-    expensive: {
-        '1': {
+    expensive: [
+         {
             name: 'The Wiggle Room',
             description: 'Voted best cocktails in Montreal, nightly burlesque shows',
             coordinates: { lat: 45.516096, long: -73.577712},
             address: '3874 St Laurent Blvd'
         },
-        '2': {
+        {
             name: 'Bootlegger',
             description: 'Fancy cocktails, live music, 1920s vibes',
             coordinates: { lat: 45.513194, long: -73.570679 },
             address: '3956 St Laurent Blvd'
         },
-        '3': {
+        {
             name: 'Tokyo Bar',
             description: 'This splashy, long-running nightspot has DJs, dancing, & cocktails featured on a hip terrace',
             coordinates: { lat: 45.515077, long: -73.574579},
             address: '3709 St Laurent Blvd'
         },
-        '4': {
+        {
             name: 'Buvette Chez Simone',
             description: 'Convivial wine bar with rotating list of vintages & tapas in urban digs with moody lighting',
             coordinates: { lat: 45.518822, long: -73.593503},
             address: '4869 Park Ave'
         }
-    },
-    cheap: {
-        '5': {
+    ],
+    cheap: [
+        {
             name: 'La P’tite grenouille',
             description: 'Festive late-night bar offering poutine, burgers & other hearty pub fare, live bands & DJs.',
             coordinates: { lat: 45.512884, long: -73.569970 },
             address: '3435 St Laurent Blvd'
         },
-        '6': {
+        {
             name: 'Blizzarts',
             description: 'Bar with themed DJ nights (ranging from reggae to rap), plus candlelit booths & dance floors.',
             coordinates: { lat: 45.516634, long: -73.578900 },
             address: '3956 St Laurent Blvd'
         },
-        '7': {
+        {
             name: 'Fitzroy',
             description: "15 tables de billard et ses 3 tables de baby-foot! Le Fitzroy, c'est le rendez-vous des joueurs de billard et le rassembleur des amateurs d’événements sportifs de tous genres",
             coordinates: { lat: 45.525248, long: -73.581954 },
             address: '551 Mont-Royal Ave E'
         },
-        '8': {
+        {
             name: 'McKibbins irish pub',
             description: 'Great service, awesome food, great prices and good selection of beer.',
             coordinates: { lat: 45.513410, long: -73.571244 },
             address: '3515 St Laurent Blvd'
         }
 
-    }
+    ]
 }
 
-let historical = {
-    '1': {
+let historical = [
+    {
         name: 'Leonard Cohens home',
         description: 'The legendary singer/poets Montreal home',
         coordinates: { lat: 45.519025, long: -73.583421},
         address: '6 Rue Vallières'
     },
-    '2': {
+    {
         name: 'Mordecai Richler Gazebo',
         description: 'The legendary Montreal authors tribute Gazebo',
         coordinates: { lat: 45.513795, long: -73.585420},
         address: '4060 Park Ave'
     },
-    '3': {
+    {
         name: 'Ancienne École des beaux-arts de Montréal',
         description: 'Labyrinthic, dilapidated space full of history.',
         coordinates: { lat: 45.511337, long: -73.570998},
         address: '3450 St Urbain St'
     }
 
-}
+]
 
 
--- back-end/restaurants.js
+++ back-end/restaurants.js
@@ -1,63 +1,63 @@
 //resto data stored here
 
 let asian = {
-    expensive: {
-        '1': {
+    expensive: [
+        {
             name: 'Sushi Momo Vegetalien',
             description: 'Rustic-chic bistro specializing in creative vegan sushi rolls & Japanese entrees.',
             coordinates: { lat: 45.518112, long: -73.569495 },
             address: '3609 St Denis Str'
         },
-        '2': {
+        {
             name: 'Mito Sushi',
             description: 'Puisqu’on ne fait rien d’ordinaire, nos Itamae ont revisité les sushis incontournables en leur ajoutant leur touche de créativité ; les subtilités de ces indispensables vous feront craquer.',
             coordinates: { lat: 45.518845, long: -73.587720 },
             address: '101 Mont-Royal Ave W'
-        }
-    },
-    cheap: {
-        '3': {
+        }]
+    ,
+    cheap: [
+        {
             name: 'Ramen Ya',
             description: 'Japanese soups, noodle dishes & sake in a casual setting with wooden touches & a wraparound counter.',
             coordinates: { lat: 45.518514, long: -73.583266 },
             address: '4274 St Laurent Blvd'
         },
-        '4': {
+        {
             name: 'Ichi Sushi',
             description: 'Extensive menu of creative sushi rolls (some in soy-paper wraps) & miso soups to take away.',
             coordinates: { lat: 45.520494, long: -73.586166 },
             address: '23 Mont-Royal Ave E'
         }
-    }
+    ]
 }
 
 let latinMex = {
-    expensive: {
-        '5': {
+    expensive: [
+        {
             name: 'Casa Minhota',
             description: 'Traditional Portuguese, select wine list & festive ambience',
             coordinates: { at: 45.516516, long: -73.578135 },
             address: '3959 St Laurent Blvd”'
 
         },
-        '6': {
+        {
             name: 'La Sala Rosa',
             description: 'Eatery serving tapas and paellas, entertainment upstairs on select nights',
-            coordinates: { lat: 45.521849, long: -73.590429},
+            coordinates: { lat: 45.521849, long: -73.590429 },
             address: '4848 St Laurent Blvd'
 
         }
 
-    },
-    cheap: {
-        '7': {
+    ],
+    cheap: [
+        {
             name: 'T&T Tacos et Tortas',
             description: 'Mexican food & Cocktail/Wine Bar',
             coordinates: { lat: 45.517468, long: -73.582390 },
             address: '51 Rachel St W'
 
         },
-        '8': {
+        {
             name: 'Rotisserie Romados',
             description: 'Montreals favourite Portuguese grilled chicken and sandwiches.',
             coordinates: { lat: 5.519285, long: -73.580757 },
@@ -65,6 +65,5 @@ let latinMex = {
 
         }
 
-    }
+    ]
 }
-
